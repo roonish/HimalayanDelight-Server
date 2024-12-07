2d3690cc5ce999d372431d04b7247939b42255b5
Ronish Shivakoti <ronishsiwakoti@gmail.com>
2024-12-07 10:06:55 -0500

    chore:Cart item added with fooditem foreign keu

--- himalayanDelightServer/models.py
+++ himalayanDelightServer/models.py
@@ -5,10 +5,15 @@ from django.core.validators import MinValueValidator
 class FoodItem(models.model):
     name = models.CharField(max_length=255)
     desc = models.TextField()
+    calory = models.PositiveSmallIntegerField()
     unit_price = models.DecimalField(max_digits=4,decimal_places=2)
 
 
 
 
 class CartItem(models.model):
+    foodItem = models.ForeignKey(FoodItem,on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField(  
+        validators=[MinValueValidator(1)])
 
+    
