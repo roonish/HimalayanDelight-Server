3ff393dd0771e8df46a7cdaadd195e29513c92c0
Ronish Shivakoti <ronishsiwakoti@gmail.com>
2024-12-09 16:38:45 -0500

    chore:admin view created and sql added

--- fooditem/models.py
+++ fooditem/models.py
@@ -2,17 +2,29 @@ from django.db import models
 from uuid import uuid4
 from django.core.validators import MinValueValidator
 
-class FoodCollection(models.model):
+class FoodCollection(models.Model):
     title = models.CharField(max_length=255)
 
-class FoodItem(models.model):
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['title']
+
+class FoodItem(models.Model):
     name = models.CharField(max_length=255)
     desc = models.TextField()
     calory = models.PositiveSmallIntegerField()
     unit_price = models.DecimalField(max_digits=4,decimal_places=2)
     collection = models.ForeignKey(FoodCollection,on_delete=models.PROTECT)
 
-class CartItem(models.model):
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['name']
+
+class CartItem(models.Model):
     foodItem = models.ForeignKey(FoodItem,on_delete=models.CASCADE)
     quantity = models.PositiveSmallIntegerField(  
         validators=[MinValueValidator(1)])
