110f8d45f2af8473a4d2048233069305c5c3fa2e
Ronish Shivakoti <ronishsiwakoti@gmail.com>
2024-12-29 21:02:56 -0500

    chore:logging configured

--- himalayanDelightServer/settings.py
+++ himalayanDelightServer/settings.py
@@ -132,3 +132,32 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version':1,
+    'disable_existing_loggers':False,
+    'handlers':{
+        'console':{
+            'class':'logging.StreamHandler'
+        },
+        'file':{
+            'class':'logging.FileHandler',
+            'filename':'general.log'
+        }
+    },
+    'loggers':{
+        '':{
+            'handlers':['console','file'],
+            'level': os.environ.get('DJANGO_LOG_LEVEL','INFO')
+
+        }
+    },
+    'formatters':{
+        'verbose':{
+            'format':'{asctime} ({levelname})-{name}-{message}',
+            'style': '{'
+        }
+    }
+
+
+}
