d188e1221cda8908d5d5bbdd961890c9d647a06a
Ronish Shivakoti <ronishsiwakoti@gmail.com>
2025-02-28 21:50:39 -0500

    feat: Fav create and list created

--- fooditem/views.py
+++ fooditem/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.viewsets import ModelViewSet,GenericViewSet
+from rest_framework.viewsets import ModelViewSet,GenericViewSet,ReadOnlyModelViewSet
 from rest_framework.filters import SearchFilter,OrderingFilter
 from rest_framework.mixins import RetrieveModelMixin
+from rest_framework.response import Response
+from rest_framework import status
 import logging
 from .models import *
 from .serializers import *
@@ -27,11 +29,39 @@ class CartItemViewSet(ModelViewSet):
     queryset = CartItem.objects.all()
     serializer_class=CartItemSerializer
 
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from .models import Favourite
+from .serializers import FavouriteSerializer
+
 class FavouritesViewSet(ModelViewSet):
     queryset = Favourite.objects.all()
-    serializer_class=FavouriteSerializer
+    serializer_class = FavouriteSerializer
     lookup_field = 'id'
 
-class RecommendationViewSet(RetrieveModelMixin,GenericViewSet):
+    def list(self, request):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)  #  Return list of favourite items
+
+    def create(self, request, *args, **kwargs):
+        food_item_id = request.data.get("food_item")  # Extract food item ID
+
+        if not food_item_id:
+            return Response({"error": "food_item ID is required"}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            food_item = FoodItem.objects.get(id=food_item_id)  # Fetch FoodItem
+        except FoodItem.DoesNotExist:
+            return Response({"error": "Food item not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        # Create the Favourite entry
+        favourite = Favourite.objects.create(foodItem=food_item)
+        serializer = self.get_serializer(favourite)
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)  # Return created objecT)
+
+
+class RecommendationViewSet(ReadOnlyModelViewSet):
     queryset = Recommendation.objects.all()
     serializer_class=RecommendationSerializer
