55485d153b85d88f5ed198dec77c1456d17fbb16
Ronish Siwakoti <ronish@mac.home>
2025-05-21 16:51:37 -0400

    fix:host added for iphone

--- himalayanDelightServer/settings/dev.py
+++ himalayanDelightServer/settings/dev.py
@@ -5,7 +5,7 @@ DEBUG = True
 
 SECRET_KEY = 'django-insecure-%=%grtsdw61dqr@u@_($+0sfc9pnk_l-lrg$v@1f(b(=1q-=b5'
 
-ALLOWED_HOSTS = ['10.0.2.2', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['10.0.2.2', '127.0.0.1', 'localhost','192.168.2.205']
 
 DATABASES = {
     'default': {
@@ -19,3 +19,5 @@ DATABASES = {
 
 # to run api locally
 # python manage.py runserver 0.0.0.0:8000
+# for ios
+# python manage.py runserver 192.168.2.205:8000
--- himalayanDelightServer/settings/prod.py
+++ himalayanDelightServer/settings/prod.py
@@ -5,4 +5,4 @@ DEBUG = False
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
-ALLOWED_HOSTS = ['10.0.2.2']
+ALLOWED_HOSTS = ['10.0.2.2','192.168.2.205']
